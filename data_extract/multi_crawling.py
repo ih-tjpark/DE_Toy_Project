b0ffc84068f6f4434a8cc2c61c74aab94841c56e
TJ Park <hahxowns@naver.com>
2025-06-09 17:46:10 +0900

    Update Crawling code
    상품 기본 정보를 추출하는 코드 추가

--- data_extract/multi_crawling.py
+++ data_extract/multi_crawling.py
@@ -1,12 +1,16 @@
-from selenium_coupang import coupang_crawling
+from selenium_coupang import get_coupang_review, get_product_links
 from multiprocessing import Pool, cpu_count
+
 
 def run_multi_process(url_list:list) -> None:
     # CPU 절반 사용
+    print("cpu개수: ",cpu_count())
     with Pool(processes=cpu_count() // 2) as pool:
-        results = pool.map(coupang_crawling())
+        pool.map(get_coupang_review, url_list)
 
 
 
 if __name__=="__main__":
-    
+    search_url = '청소기'
+    product_link_list = get_product_links()
+    run_multi_process(product_link_list[:3])
--- data_extract/selenium_coupang.py
+++ data_extract/selenium_coupang.py
@@ -63,38 +63,24 @@ def go_next_page(driver : uc.Chrome , page_num:int, review_id:str) -> bool:
     
 def save_reviews_to_parquet(reviews : list) -> None:
     df = pd.DataFrame(reviews)
-    file_path = "DE_Toy_Project/review_data_save/coupang_reviews.parquet"
+    name = reviews[0]['name']
+    file_path = f"DE_Toy_Project/review_data_save/coupang_review_{name}.parquet"
     df.to_parquet(file_path, engine="pyarrow", index=False)
-    print(f"[INFO] 리뷰가 parquet 파일로 저장되었습니다")
+    print(f"[INFO] {name} 리뷰가 parquet 파일로 저장되었습니다")
 
-def coupang_crawling(product_url : str) -> None:
+def get_coupang_review(product_url : str) -> None:
     try:
         driver = setup_driver()
         driver.get(product_url)
-        time.sleep(random.uniform(3, 4))
-        #actions = driver.find_element(By.CSS_SELECTOR, 'body')
-        #actions.send_keys(Keys.PAGE_DOWN)
-        print("url 접근 성공")
-    except Exception as e:
-        print("url 접근 실패:", e)
-
-    try:    
+        time.sleep(random.uniform(5, 6))
         try:
-            productSelect = driver.find_element(By.XPATH, '/html/body/div[3]/section/form/div[2]/div[2]/ul/li[1]/a')
-        except NoSuchElementException:
-            productSelect = driver.find_element(By.XPATH, '/html/body/div[2]/section/form/div[2]/div[2]/ul/li[1]/a')
-            print("url path2로 링크 찾음")
-
-        driver.get(productSelect.get_attribute("href"))
-        time.sleep(random.uniform(4, 5))
-        
-        try:
+            name = driver.find_element(By.CSS_SELECTOR, '[data-sentry-component="ProductTitle"]').text
             name = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[1]/div[4]/div[1]/div[3]/div[1]/h1/span').text
             #price = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[1]/div[4]/div[2]/div[3]/div[1]')
         except NoSuchElementException:
             name = driver.find_element(By.XPATH, '/html/body/div[2]/section/div[2]/div[1]/div[3]/div[3]/h1').text
-            print("title path2로 링크 찾음")
-        print(name)
+            #print("title path2로 링크 찾음")
+        #print(name)
 
         #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -112,28 +98,86 @@ def coupang_crawling(product_url : str) -> None:
                     rating = article.find_element(By.CSS_SELECTOR, '[data-rating]').get_attribute("data-rating")
                     date = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__info__product-info__reg-date').text
                     content = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__review__content').text
-                    print(f" {rating} /  {date} / 내용: {content[:50]}")
+                    #print(f" {rating} /  {date} / 내용: {content[:50]}")
                 
-                product_list.append({'name':name, 'rating':rating, 'date':date, 'content':content})
+                product_list.append({
+                    'name':name, 
+                    'rating':rating, 
+                    'date':date, 
+                    'content':content
+                    })
             except Exception as e:
-                    print("리뷰 항목 추출 실패:", e) 
+                    print("리뷰 항목 추출 실패:", e)
+                    continue
             
             next_page_success = go_next_page(driver, p+1, review_id)
             if not next_page_success:
                 break
+
         save_reviews_to_parquet(product_list)
+        driver.quit()
     except Exception as e:
         print("리뷰 추출 실패:", e)
-        
-def product_link_crawling(url : str) -> list:
-    
-    
+    # finally:
+        # driver.quit()
+
+def get_product_links(keyword="청소기", max_links=3):
+
     driver = setup_driver()
-    driver.get('product_url')
-    return [] 
+
+    search_url = f"https://www.coupang.com/np/search?component=&q={keyword}"
+    driver.get(search_url)
+
+    time.sleep(random.uniform(3, 4))
+
+    links = []
+    items = driver.find_elements(By.CSS_SELECTOR, '#product-list li')
+    
+    for item in items:
+        
+        # 링크 주소
+        href = item.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        # 이미지 주소
+        img = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        # 상품명
+        name = item.find_elements(By.TAG_NAME, 'div')[2].text
+        # 가격
+        try:
+            origin_price = item.find_element(By.TAG_NAME, 'del').text
+        except NoSuchElementException:
+            origin_price = 0
+        price = item.find_element(By.TAG_NAME, 'strong').text
+        
+        # 상품 스타 랭크, 리뷰 수
+        try:
+            product_info = item.find_elements(By.CSS_SELECTOR, '[data-sentry-component="ProductRating"] span')
+        except NoSuchElementException:
+            star_rating = 0
+            review_count = 0
+        try:
+            star_rating = product_info[0].find_element(By.CSS_SELECTOR, 'div').text
+        except NoSuchElementException:
+            star_rating = 0
+        try:
+            review_count = product_info[1].text
+        except NoSuchElementException:
+            review_count = 0
+        
+        print(name)
+        print(img)
+        print(price)
+        print(origin_price)
+        print(star_rating)
+        print(review_count)
+        links.append(href)
+        if len(links) >= max_links:
+            break
+    print(f"[INFO] {len(links)}개 상품 url 추출 완료.")        
+    #driver.quit()
+    return links
 
 if __name__ == "__main__":
-    product_url = 'https://www.coupang.com/np/search?component=&q=청소기'
-    coupang_crawling(product_url)
+    product_url = ''
+    product_url_list = get_product_links()
+    get_coupang_review(product_url_list[0])
 
-#driver.quit()
