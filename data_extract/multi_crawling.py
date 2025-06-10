401128c66501bd418b641bf6be590ddd87ed49e2
TJ Park <hahxowns@naver.com>
2025-06-10 17:52:03 +0900

    Update Crawling Code
    상세 페이지에서 상품에 대한 기본 정보 데이터 추출하도록 변경

--- data_extract/multi_crawling.py
+++ data_extract/multi_crawling.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool, cpu_count
 def run_multi_process(url_list:list) -> None:
     # CPU 절반 사용
     print("cpu개수: ",cpu_count())
-    with Pool(processes=cpu_count() // 2) as pool:
+    with Pool(processes=2) as pool:
         pool.map(get_coupang_review, url_list)
 
 
@@ -13,4 +13,4 @@ def run_multi_process(url_list:list) -> None:
 if __name__=="__main__":
     search_url = '청소기'
     product_link_list = get_product_links()
-    run_multi_process(product_link_list[:3])
+    run_multi_process(product_link_list)
--- data_extract/selenium_coupang.py
+++ data_extract/selenium_coupang.py
@@ -1,3 +1,4 @@
+import re
 import time
 import random
 import pandas as pd
@@ -20,28 +21,42 @@ def setup_driver() -> uc.Chrome:
     random_ua = UserAgent().random
     options.add_argument('user-agent=' + random_ua)
 
-    driver = uc.Chrome(options=options, enable_cdp_events=True, incognito=True)
-    return driver
+    return uc.Chrome(driver_executable_path='C:\\Users\\student\\AppData\\Roaming\\undetected_chromedriver\\undetected_chromedriver.exe',
+                     options=options, enable_cdp_events=True, incognito=True)
 
-def check_element(xP : str, driver : uc.Chrome) -> bool:
+def check_element(xP: str, driver: uc.Chrome) -> bool:
     try:
         return driver.find_element(By.XPATH, xP).is_enabled()
     except:
         return False
 
-def check_element_css(css : str, driver : uc.Chrome) -> bool:
+def check_element_css(css: str, driver: uc.Chrome) -> bool:
     try:
         return driver.find_element(By.CSS_SELECTOR, css).is_enabled()
     except:
         return False
 
-def go_next_page(driver : uc.Chrome , page_num:int, review_id:str) -> bool:
+def get_product_code(url: str) -> str:
+    prod_code: str = url.split("products/")[-1].split("?")[0]
+    return prod_code
+
+def get_star_rating(element: str) -> float: 
+    rating_percent = float(re.sub(r'[^0-9]', '', element))
+    avg_rating = round((rating_percent / 20), 2)  # 20% → ★1, 100% → ★5
+    return avg_rating
+
+def get_review_count(element: str) -> int:
+    review_count = int(re.sub(r'[^0-9]', '', element))
+    return review_count
+
+def go_next_page(driver: uc.Chrome , page_num: int, review_id: str) -> bool:
     try:
         if review_id == "sdpReview":
             page_buttons = driver.find_element(By.XPATH, f'//*[@id="sdpReview"]/div/div[4]/div[2]/div/button[{page_num}]')
         else:
             page_buttons = driver.find_element(By.XPATH, f'//*[@id="btfTab"]/ul[2]/li[2]/div/div[6]/section[4]/div[3]/button[{page_num}]')
         
+        # 처음 페이지 버튼을 누를 시 화면에 노출되야 클릭됨
         if page_num <= 3:
             driver.execute_script("arguments[0].scrollIntoView(true);", page_buttons)
             time.sleep(0.5)
@@ -50,64 +65,134 @@ def go_next_page(driver : uc.Chrome , page_num:int, review_id:str) -> bool:
 
         page_buttons.click()                               
         time.sleep(random.uniform(2,3))
-        print(f"[INFO] {page_num-1} 페이지 이동")
+        #print(f"[INFO] {product_code} 리뷰 {page_num-1} 페이지 이동")
         return True
     
-    except NoSuchElementException:
-        print(f"[INFO] {page_num-1} 페이지 버튼 없음.")
+    # except NoSuchElementException:
+    #     print(f"[INFO] 리뷰 {page_num-1} 페이지 버튼 없음.")
+    #     return False
+    except:
+        print(f"[INFO] 리뷰 {page_num-1} 페이지 버튼 없음.")
         return False
     
-    except Exception as e:
-        print(f"[ERROR] 페이지 {page_num-1} 이동 실패:", e)
-        return False
-    
-def save_reviews_to_parquet(reviews : list) -> None:
+def save_reviews_to_parquet(reviews: list) -> None:
     df = pd.DataFrame(reviews)
-    name = reviews[0]['name']
-    file_path = f"DE_Toy_Project/review_data_save/coupang_review_{name}.parquet"
+    product_code = reviews[0]['product_code']
+    file_path = f"DE_Toy_Project/review_data_save/coupang_review_{product_code}.parquet"
     df.to_parquet(file_path, engine="pyarrow", index=False)
-    print(f"[INFO] {name} 리뷰가 parquet 파일로 저장되었습니다")
+    print(f"[INFO] {product_code} 리뷰가 parquet 파일로 저장되었습니다")
 
-def get_coupang_review(product_url : str) -> None:
+def get_product_info(driver: uc.Chrome) -> dict:
+    try:
+        product_dict = dict()
+        
+        # 상품 판매 제목
+        title = driver.find_element(By.CSS_SELECTOR, '[data-sentry-component="ProductTitle"]').text
+        product_dict['title'] = title
+
+        # 카테고리 추출
+        try:
+            categorys = driver.find_elements(By.CSS_SELECTOR, '[data-sentry-component="Breadcrumb"] a')
+            for i in range(1, len(categorys)):
+                product_dict[f'category{i}'] = categorys[i].text
+                #print('category:',categorys[i].text)
+        except NoSuchElementException as e:
+            print("[ERROR] 카테고리 추출 실패:",e)
+        
+        # 상품명 추출
+        try:
+            name = driver.find_element(By.CSS_SELECTOR, '#itemBrief > table > tbody > tr:nth-child(1) > td:nth-child(2)').text
+            product_dict['name'] = name
+            #print('name:', name)
+        except NoSuchElementException as e:
+            name = ''
+            print("[ERROR] 상품명 추출 실패:",e)
+        
+        # 상품 코드 추출
+        product_code = get_product_code(driver.current_url)
+        product_dict['product_code'] = product_code
+        print(product_code)
+
+        # 별점 추출
+        try:
+            el = driver.find_element(By.CSS_SELECTOR, 'span.rating-star-num').get_attribute("style")
+            star_rating = get_star_rating(el)
+            product_dict['star_rating'] = star_rating
+            #print(star_rating)
+        except NoSuchElementException as e:
+            star_rating = 0.0
+            print("[INFO] 별점 없음")
+
+        # 리뷰 수 추출
+        try:
+            el = driver.find_element(By.CSS_SELECTOR, 'span.rating-count-txt').text
+            review_count = get_review_count(el)
+            product_dict['review_count'] = review_count
+            #print('review_count:',review_count)
+        except NoSuchElementException as e:
+            review_count = ''
+            print("[INFO] 리뷰 수 없음")
+
+        # 할인 전 가격 추출
+        try:
+            sales_price = driver.find_element(By.CSS_SELECTOR, 'div.price-amount.sales-price-amount').text
+            product_dict['sales_price'] = sales_price
+            #print('sales_price:',sales_price)
+        except NoSuchElementException as e:
+            sales_price = ''
+            print("[INFO] 할인 전 가격 없음")
+        # 할인 후 가격 추출
+        try:
+            final_price = driver.find_element(By.CSS_SELECTOR, 'div.price-amount.final-price-amount').text
+            product_dict['final_price'] = final_price
+            #print('final_price:',final_price)
+        except NoSuchElementException as e:
+            sales_price = ''
+            print("[INFO] 할인 후 가격 없음")
+        
+        # DB에 저장할 dict
+        print(product_dict)
+        return product_dict
+    except Exception as e:
+        print("[ERROR] 상품 기본 정보 추출 실패:",e)
+        return product_dict
+
+def get_coupang_review(product_url: str) -> None:
     try:
         driver = setup_driver()
         driver.get(product_url)
         time.sleep(random.uniform(5, 6))
-        try:
-            name = driver.find_element(By.CSS_SELECTOR, '[data-sentry-component="ProductTitle"]').text
-            name = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[1]/div[4]/div[1]/div[3]/div[1]/h1/span').text
-            #price = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/main/div[1]/div[4]/div[2]/div[3]/div[1]')
-        except NoSuchElementException:
-            name = driver.find_element(By.XPATH, '/html/body/div[2]/section/div[2]/div[1]/div[3]/div[3]/h1').text
-            #print("title path2로 링크 찾음")
-        #print(name)
 
-        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+        # 상품 기본 정보 추출
+        product_dict = get_product_info(driver)
+        product_code = product_dict['product_code']
+        # 기본 정보 DB 저장
+        
+        # 리뷰 추출
         if check_element_css("#sdpReview article", driver):
             review_id = "sdpReview"
         else:
             review_id = "btfTab"
 
         product_list = []
-        for p in range(2,5):
+        for p in range(2,10):
             try:
                 articles = driver.find_elements(By.CSS_SELECTOR, f"#{review_id} article")
 
                 for article in articles:
-                    rating = article.find_element(By.CSS_SELECTOR, '[data-rating]').get_attribute("data-rating")
-                    date = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__info__product-info__reg-date').text
-                    content = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__review__content').text
-                    #print(f" {rating} /  {date} / 내용: {content[:50]}")
+                    review_rating = article.find_element(By.CSS_SELECTOR, '[data-rating]').get_attribute("data-rating")
+                    review_date = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__info__product-info__reg-date').text
+                    review_content = article.find_element(By.CSS_SELECTOR, 'div.sdp-review__article__list__review__content').text
+                    #print(f" 별점:{rating} /  등록 날짜:{date} / 내용: {content[:50]}")
                 
                 product_list.append({
-                    'name':name, 
-                    'rating':rating, 
-                    'date':date, 
-                    'content':content
+                    'product_code':product_code, 
+                    'review_rating':review_rating, 
+                    'review_date':review_date, 
+                    'review_content':review_content
                     })
             except Exception as e:
-                    print("리뷰 항목 추출 실패:", e)
+                    print(f"[INFO] {product_code}리뷰 항목 추출 실패:", e)
                     continue
             
             next_page_success = go_next_page(driver, p+1, review_id)
@@ -115,66 +200,79 @@ def get_coupang_review(product_url : str) -> None:
                 break
 
         save_reviews_to_parquet(product_list)
+        print(f'[INFO] {product_code} 리뷰 추출을 완료했습니다.')
         driver.quit()
     except Exception as e:
-        print("리뷰 추출 실패:", e)
-    # finally:
-        # driver.quit()
+        print(f"[ERROR] {product_code} 에러 발생 :", e)
+    finally:
+        driver.quit()
 
-def get_product_links(keyword="청소기", max_links=3):
+def get_product_links(keyword="청소기", max_links=10):
 
     driver = setup_driver()
-
     search_url = f"https://www.coupang.com/np/search?component=&q={keyword}"
     driver.get(search_url)
-
     time.sleep(random.uniform(3, 4))
 
     links = []
-    items = driver.find_elements(By.CSS_SELECTOR, '#product-list li')
+    try:
+        items = driver.find_elements(By.CSS_SELECTOR, '#product-list li')
+    except NoSuchElementException as e:
+        print("[INFO] 검색된 상품이 없습니다.:", e)
+        return []
     
-    for item in items:
-        
-        # 링크 주소
-        href = item.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        # 이미지 주소
-        img = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
-        # 상품명
-        name = item.find_elements(By.TAG_NAME, 'div')[2].text
-        # 가격
-        try:
-            origin_price = item.find_element(By.TAG_NAME, 'del').text
-        except NoSuchElementException:
-            origin_price = 0
-        price = item.find_element(By.TAG_NAME, 'strong').text
-        
-        # 상품 스타 랭크, 리뷰 수
-        try:
-            product_info = item.find_elements(By.CSS_SELECTOR, '[data-sentry-component="ProductRating"] span')
-        except NoSuchElementException:
-            star_rating = 0
-            review_count = 0
-        try:
-            star_rating = product_info[0].find_element(By.CSS_SELECTOR, 'div').text
-        except NoSuchElementException:
-            star_rating = 0
-        try:
-            review_count = product_info[1].text
-        except NoSuchElementException:
-            review_count = 0
-        
-        print(name)
-        print(img)
-        print(price)
-        print(origin_price)
-        print(star_rating)
-        print(review_count)
-        links.append(href)
-        if len(links) >= max_links:
-            break
-    print(f"[INFO] {len(links)}개 상품 url 추출 완료.")        
-    #driver.quit()
-    return links
+    try:
+        for item in items:
+            
+            # 링크 주소
+            href = item.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            # # 이미지 주소
+            # img = item.find_element(By.TAG_NAME, 'img').get_attribute('src')
+            # # 상품 제목
+            # title = item.find_elements(By.TAG_NAME, 'div')[2].text
+            # # 최종 가격
+            # price = item.find_element(By.TAG_NAME, 'strong').text
+            # # 원래 가격
+            # try:
+            #     origin_price = item.find_element(By.TAG_NAME, 'del').text
+            # except NoSuchElementException:
+            #     origin_price = 0
+            # 상품 별점, 리뷰 수
+            try:
+                product_info = item.find_elements(By.CSS_SELECTOR, '[data-sentry-component="ProductRating"] span')
+            except NoSuchElementException:
+                star_rating = 0
+                review_count = 0
+            try:
+                star_rating = product_info[0].find_element(By.CSS_SELECTOR, 'div').text
+            except NoSuchElementException:
+                star_rating = 0
+            try:
+                review_count = get_review_count(product_info[1].text)
+            except:
+                review_count = 0
+            
+            # 특정 개수 이상의 리뷰가 있는 상품만 가져오기
+            if review_count >= 200:
+                links.append(href)
+            
+            # print('title:',title)
+            # print('href:',href)
+            # print('img:',img)
+            # print('price:',price)
+            # print('origin_price:',origin_price)
+            # print('star_rating:',star_rating)
+            # print('review_count:',review_count)
+            if len(links) >= max_links:
+                break
+        print(f"[INFO] {len(links)}개 상품 url 추출 완료.")        
+        #driver.quit()
+        return links
+    except Exception as e:
+        print("[ERROR] 상품 url 추출 실패.:", e)
+        return []
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
     product_url = ''
